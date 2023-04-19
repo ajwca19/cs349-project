170a492259b393afa6015de28ea80f0a366f5c95
ajwca19 <ajwca19@gmail.com>
2023-04-19 15:06:48 -0500

    addressing data preprocessing

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -19,8 +19,18 @@ def data_preprocessing(path, test=False):
     #extract files as pandas dataframes
     product_df = pd.read_json(pfilename)
     review_df = pd.read_json(rfilename)
+    grouped_reviews = review_df.groupby('asin')
     
-    
+    asin_review_data_dict = {}
+    for ind in product_df.index:
+        review_group = grouped_reviews.get_group(review_df['asin'][ind])
+        asin_review_data_dict.update({product_df['asin'][ind] : review_group})
+        
+    return asin_review_data_dict
     
 if __name__ == '__main__':
-    data_preprocessing("devided_dataset_v2/Grocery_and_Gourmet_Food/test1", True)
+    # dictionary of ASINS and the reviews for the ASIN
+    asin_review_data_train = data_preprocessing("../devided_dataset_v2/CDs_and_Vinyl/train", False)
+
+# list of ASINS (keys for the dictionary)
+asins = list(asin_review_data_train.keys())
